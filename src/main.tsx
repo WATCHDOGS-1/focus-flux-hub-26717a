5da4392df639df5041e93069940269741b7270ac
[dyad] <watchdogsgvaway@gmail.com>
2025-11-08 22:58:26 +0530

    [dyad] wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+// The CSS import is moved to index.html for non-blocking loading.
+// import "./index.css"; 
 
 createRoot(document.getElementById("root")!).render(<App />);
