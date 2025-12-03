afb720bb91cbcce5117870917a31a10930cee660
[dyad] <watchdogsgvaway@gmail.com>
2025-12-03 23:55:07 +0530

    [dyad] Removed the YouTube player from the Notes and Media workspace panel. - wrote 1 file(s)

--- src/components/NotesAndMediaPanel.tsx
+++ src/components/NotesAndMediaPanel.tsx
@@ -1,39 +1,23 @@
 import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 import NotesWorkspace from "./NotesWorkspace";
 import TaskList from "./TaskList";
-import YouTubePanel from "./YouTubePanel";
 
 const NotesAndMediaPanel = () => {
   return (
     <div className="h-full w-full">
-      <PanelGroup direction="vertical" className="h-full">
-        
-        {/* Top Panel: Notes and Tasks (Resizable Horizontal Split) */}
-        <Panel defaultSize={50} minSize={30}>
-          <PanelGroup direction="horizontal" className="h-full">
-            {/* Notes Workspace (70%) */}
-            <Panel defaultSize={70} minSize={30}>
-              <NotesWorkspace />
-            </Panel>
-            
-            <PanelResizeHandle className="w-2 flex items-center justify-center bg-border/50 hover:bg-primary/50 transition-colors cursor-col-resize">
-              <div className="w-1 h-10 bg-primary/50 rounded-full" />
-            </PanelResizeHandle>
-            
-            {/* Task List (30%) */}
-            <Panel defaultSize={30} minSize={20}>
-              <TaskList />
-            </Panel>
-          </PanelGroup>
+      <PanelGroup direction="horizontal" className="h-full">
+        {/* Notes Workspace (70%) */}
+        <Panel defaultSize={70} minSize={30}>
+          <NotesWorkspace />
         </Panel>
         
-        <PanelResizeHandle className="h-2 flex items-center justify-center bg-border/50 hover:bg-primary/50 transition-colors cursor-row-resize">
-          <div className="h-1 w-10 bg-primary/50 rounded-full" />
+        <PanelResizeHandle className="w-2 flex items-center justify-center bg-border/50 hover:bg-primary/50 transition-colors cursor-col-resize">
+          <div className="w-1 h-10 bg-primary/50 rounded-full" />
         </PanelResizeHandle>
         
-        {/* Bottom Panel: YouTube Player */}
-        <Panel defaultSize={50} minSize={20}>
-          <YouTubePanel />
+        {/* Task List (30%) */}
+        <Panel defaultSize={30} minSize={20}>
+          <TaskList />
         </Panel>
       </PanelGroup>
     </div>
